45dc9a75a87244cd0e6832ca646ff2ffab4c97ca
Megan-Morales <meganm.beta@gmail.com>
2022-04-08 18:27:09 -0600

    Creación del proyecto

---
+++ LAB5_ED1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LAB5_ED1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
