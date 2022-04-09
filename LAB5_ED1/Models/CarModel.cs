bc851df5017f3ba6c3a93e513c1582e7402ef5aa
Megan-Morales <meganm.beta@gmail.com>
2022-04-08 22:37:05 -0600

    Agregar el modelo

---
+++ LAB5_ED1/Models/CarModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB5_ED1.Models
+{
+    public class CarModel
+    {
+    }
+}
---
+++ LAB5_ED1/Árbol2-3/NodoPagina.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LAB5_ED1.Árbol2_3
+{
+    public class NodoPagina
+    {
+    }
+}
