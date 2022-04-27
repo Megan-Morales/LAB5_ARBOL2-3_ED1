26bdfb2abec8f10cf47135604da08cc7400cc47b
Betancourt Jimenez <sebetanc@sat.gob.gt>
2022-04-27 13:05:02 -0600

    Actualizar método de búsqueda en el proyecto de árbol multicamino

--- Arboles multiamino/Controllers/HomeController.cs
+++ Arboles multiamino/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Arboles_multiamino.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult IndexViewProject()
         {
             return View();
         }
