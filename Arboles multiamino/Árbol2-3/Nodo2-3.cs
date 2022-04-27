63075e88572ab2890f90009abf4663e773c861f8
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-04-27 09:56:41 -0600

    added new project fro 23 tree

---
+++ Arboles multiamino/Árbol2-3/Nodo2-3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LAB5_ED1.Árbol2_3
+{
+    public class Nodo2_3<T> where T : IComparable
+    {
+        public T valor; //->Valor/ clave 
+        //Apuntadores
+        public Nodo2_3<T> anterior, siguiente;
+        public Pagina<T> hijoIzq, hijoDer;
+
+        public Nodo2_3(T valor) //contructor de mi clase 
+        {
+            this.valor = valor;
+            this.anterior = this.siguiente = null;
+            this.hijoIzq = this.hijoDer = null;
+        }
+
+        public Nodo2_3(T valor, Pagina<T> izquierda, Pagina<T> derecha) //Segundo constructor, aplicar sobre carga de constructores 
+        {
+            this.valor = valor;
+            this.anterior = this.siguiente = null;
+            this.hijoIzq = izquierda;
+            this.hijoDer = derecha;
+        }
+    }
+}
